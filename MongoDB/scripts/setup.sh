554fe64dd52ae747c8033877610cd8d54e5b14b4
jackson <jackson@jacksondeMacBook-Pro.local>
2020-11-15 00:06:06 +0800

    add mongos

--- MongoDB/scripts/setup.sh
+++ MongoDB/scripts/setup.sh
@@ -1,24 +1,23 @@
-#!/bin/bash 
+#!/bin/bash
 
-mongodb1=`getent hosts ${MONGO1} | awk '{ print $1 }'`
-mongodb2=`getent hosts ${MONGO2} | awk '{ print $1 }'`
-mongodb3=`getent hosts ${MONGO3} | awk '{ print $1 }'`
+mongodb1=$(getent hosts ${MONGO1} | awk '{ print $1 }')
+mongodb2=$(getent hosts ${MONGO2} | awk '{ print $1 }')
+mongodb3=$(getent hosts ${MONGO3} | awk '{ print $1 }')
 
 port=${PORT:-27017}
 
 echo "Waiting for startup.."
 until mongo --host ${mongodb1}:${port} --eval 'quit(db.runCommand({ ping: 1 }).ok ? 0 : 2)' &>/dev/null; do
-  printf '.'
-  sleep 1
+    printf '.'
+    sleep 1
 done
 
 echo "Started.."
 
-echo setup.sh time now: `date +"%T" `
+echo setup.sh time now: $(date +"%T")
 mongo --host ${mongodb1}:${port} -uroot -p${PASSWORD} --authenticationDatabase admin <<EOF
    var cfg = {
         "_id": "${RS}",
-        "protocolVersion": 1,
         "members": [
             {
                 "_id": 0,
