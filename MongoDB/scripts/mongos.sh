93066557844ded35f9e56dde133b5d05e5883092
jacksoncy <wxw198812@gamil.com>
2020-11-15 11:33:26 +0800

    mongos use admin

--- MongoDB/scripts/mongos.sh
+++ MongoDB/scripts/mongos.sh
@@ -2,7 +2,7 @@
 
 mongodb1=$(getent hosts ${MONGO1} | awk '{ print $1 }')
 
-port=${PORT:-27017}
+port=${PORT:-27018}
 
 echo "Waiting for startup.."
 until mongo --host ${mongodb1}:${port} --eval 'quit(db.runCommand({ ping: 1 }).ok ? 0 : 2)' &>/dev/null; do
@@ -13,4 +13,6 @@ done
 echo "Started.."
 
 echo setup.sh time now: $(date +"%T")
-mongo --host ${mongodb1}:${port} --eval 'sh.addShard("BigBoss/172.18.5.10:27017,172.18.5.11:27017,172.18.5.12:27017")'
+mongo --host ${mongodb1}:${port} --authenticationDatabase admin <<EOF
+    sh.addShard("BigBoss/172.18.5.10:27017,172.18.5.11:27017,172.18.5.12:27017")
+EOF
