9103cff4fa2aaff57c8f970684d7dfbc425b8a53
jacksoncy <wxw198812@gamil.com>
2020-11-15 16:14:04 +0800

    mongod port

--- MongoDB/before.sh
+++ MongoDB/before.sh
@@ -9,14 +9,14 @@ chown 999 keyfile/mongo.key
 docker-compose up -d 
 
 ## Config servers setup
-docker exec -it mongo-configsvr0 sh -c "mongo --port 27017 < /scripts/configserver.js"
+docker exec -it mongo-configsvr0 sh -c "mongo --port 27019 < /scripts/configserver.js"
 
 ## Shard servers setup
-docker exec -it mongodb-primary sh -c "mongo --port 27018 < /scripts/shard.js"
+docker exec -it mongodb-primary sh -c "mongo --port 27017 < /scripts/shard.js"
 
 ## Apply sharding configuration
 sleep 15
-docker exec -it mongos sh -c "mongo --port 27017 < /scripts/mongos.js"
+docker exec -it mongos sh -c "mongo --port 27018 < /scripts/mongos.js"
 
 ## Enable admin account
 #docker exec -it mongodbdocker_mongo-router-01_1 sh -c "mongo --port 27017 < /mongo-auth.init.js"
