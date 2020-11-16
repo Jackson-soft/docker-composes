24c5f45a5e230c6829c6f26cd03c1da15c1b8352
jacksoncy <wxw198812@gamil.com>
2020-11-16 20:28:26 +0800

    change redis

--- Kurento/start.sh
+++ Kurento/start.sh
@@ -4,6 +4,4 @@ if [ -z "$REAL_EXTERNAL_IP" ]; then
 	export REAL_EXTERNAL_IP="$(curl -4 https://icanhazip.com 2>/dev/null)"
 fi
 
-exec echo "$REAL_EXTERNAL_IP"
-
-sudo docker-compose up -d
+docker-compose up -d
