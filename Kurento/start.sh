4bafb2bc90ac6b905ffc03de2434cc2ff5164d97
jacksoncy <wxw198812@gamil.com>
2020-11-16 17:32:32 +0800

    add kurento ip

---
+++ Kurento/start.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if [ -z "$REAL_EXTERNAL_IP" ]; then
+	export REAL_EXTERNAL_IP="$(curl -4 https://icanhazip.com 2>/dev/null)"
+fi
+
+exec echo "$REAL_EXTERNAL_IP"
+
+sudo docker-compose up -d
