edc198352d6d00571da98bc6b5ff65775fdec9cd
jacksoncy <wxw198812@gamil.com>
2020-11-23 14:15:58 +0800

    database

--- MongoDB/scripts/auth.js
+++ MongoDB/scripts/auth.js
@@ -1,2 +1,9 @@
+rs.initiate({
+    _id: "BigBoss",
+    version: 1,
+    members: [
+        {_id: 0, host: "mongodb:27017"},
+    ],
+});
 db.createUser({user: "sa", pwd: "f9bd44806614!", roles: [{role: "root", db: "admin"}]});
 db.auth("sa", "f9bd44806614!");
--- MongoDB/scripts/shard.js
+++ MongoDB/scripts/shard.js
@@ -3,7 +3,5 @@ rs.initiate({
     version: 1,
     members: [
         {_id: 0, host: "mongodb-primary:27017"},
-        {_id: 1, host: "mongodb-secondary0:27017"},
-        {_id: 2, host: "mongodb-secondary1:27017"},
     ],
 });
