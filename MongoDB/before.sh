a40a8a2f4ff576bbb7cb4b97320395c4602b5c77
jacksoncy <wxw198812@gamil.com>
2020-11-23 12:54:07 +0800

    add auth

--- MongoDB/before.sh
+++ MongoDB/before.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
 
-## Generate global auth key between cluster nodes
-openssl rand -base64 756 > keyfile/mongo.key
-chmod 400 keyfile/mongo.key
-chown 999 keyfile/mongo.key
-
 ## Start the whole stack
-docker-compose up -d 
+docker-compose up -d
 
-## Config servers setup
-docker exec -it mongo-configsvr0 sh -c "mongo --port 27019 < /scripts/configserver.js"
-
-## Shard servers setup
-docker exec -it mongodb-primary sh -c "mongo --port 27017 < /scripts/shard.js"
-
-## Apply sharding configuration
-sleep 15
-docker exec -it mongos sh -c "mongo --port 27018 < /scripts/mongos.js"
-
-## Enable admin account
-#docker exec -it mongodbdocker_mongo-router-01_1 sh -c "mongo --port 27017 < /mongo-auth.init.js"
+docker exec -it mongodb sh -c "mongo --port 27017 -uroot -p12^xH0 --authenticationDatabase admin < /scripts/auth.js"
---
+++ MongoDB/scripts/auth.js
@@ -0,0 +1,2 @@
+db.createUser({user: "sa", pwd: "f9bd44806614!", roles: [{role: "root", db: "admin"}]});
+db.auth("sa", "f9bd44806614!");
