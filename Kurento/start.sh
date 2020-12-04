47f2bfd6f7de47a54b6a375117d93d7b634fc3cd
jacksoncy <wxw198812@gamil.com>
2020-12-04 14:42:38 +0800

    fix export

--- Kurento/start.sh
+++ Kurento/start.sh
@@ -2,6 +2,7 @@
 
 if [ -z "$REAL_EXTERNAL_IP" ]; then
     echo "export REAL_EXTERNAL_IP=\"$(curl -4 https://icanhazip.com 2>/dev/null)\"" >>/etc/profile
+    . /etc/profile
 fi
 
 docker-compose up -d
