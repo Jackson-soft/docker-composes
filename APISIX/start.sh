fa3f93fba9f576fd495934c3cca2aee94fa903af
jacksoncy <wxw198812@gamil.com>
2021-01-09 16:28:44 +0800

    apisix change

--- APISIX/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-docker-compose -p etcd -f docker-compose-etcd.yml up -d
-
-docker-compose -p dashboard -f docker-compose-dashboard.yml up -d
-
-docker-compose -p apisix -f docker-compose.yml up -d
